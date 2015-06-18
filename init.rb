cdacd055abf9d070be977aab87c02cfff17e00e8
Teresa <teresa.nuzzo@gmail.com>
2015-06-18 05:49:33 -0400

    showing optional link in application menu

--- init.rb
+++ init.rb
@@ -16,8 +16,16 @@ Redmine::Plugin.register :wiki_external do
   # user-accessible global configuration
   settings :default => {
     'wiki_base_url' => nil,
-    'wiki_project_custom_suffix_field' => nil
+    'wiki_project_custom_suffix_field' => nil,
+    'show_wiki_home_in_app_menu' => false,
+    'wiki_home_caption' => :project_module_wiki
   }, :partial => 'settings/wiki_external_settings'
+  
+  
+  Redmine::MenuManager.map :application_menu do |menu|
+    menu.push :wikis, { :controller => 'wikis', :action => 'index' }, :caption => Settings.plugin_wiki_external['wiki_home_caption'], :if => Proc.new { (Setting.plugin_wiki_external['show_wiki_home_in_app_menu'] == "1")}
+  end
+
   
   # Send patches to models and controllers
   Rails.configuration.to_prepare do
